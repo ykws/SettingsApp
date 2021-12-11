013fd4f43c225566d298dc661bfdd92d2bd9f5cf
KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>
2021-12-12 01:46:35 +0900

    Add Touchable UISlider

---
+++ SettingsApp/View/TouchableSlider/TouchableSlider.h
@@ -0,0 +1,16 @@
+//
+//  TouchableSlider.h
+//  SettingsApp
+//
+//  Created by KAWASHIMA Yoshiyuki on 2021/12/12.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TouchableSlider : UISlider
+
+@end
+
+NS_ASSUME_NONNULL_END
